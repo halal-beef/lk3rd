14182c498721f1e6ec335eed5de1e0a87941a630
Umer Uddin <umer.uddin@mentallysanemainliners.org>
2026-04-19 10:36:05 +0100

    Change LK base address to S-LK base
    Signed-off-by: Umer Uddin <umer.uddin@mentallysanemainliners.org>

--- platform/exynos9830/include/platform/exynos9830.h
+++ platform/exynos9830/include/platform/exynos9830.h
@@ -62,7 +62,7 @@
 #define DRAM_BASE2					0x880000000
 
 #define CFG_FASTBOOT_MMC_BUFFER				(0xC2000000)
-#define CONFIG_LK_TEXT_BASE				(0xe1900000)
+#define CONFIG_LK_TEXT_BASE				(0xE8000000)
 
 /* iRAM information */
 #define IRAM_BASE					(0x02020000)
