21f387c293ffac758c0bf2a9d619e47a3e3217ba
Umer Uddin <umer.uddin@mentallysanemainliners.org>
2026-08-06 19:49:17 +0100

    Document what is being written to the PMU register that makes SPEEDY work
    Signed-off-by: Umer Uddin <umer.uddin@mentallysanemainliners.org>

--- dev/power/pmic/s2mps_19_22/pmic_s2mps_19_22.c
+++ dev/power/pmic/s2mps_19_22/pmic_s2mps_19_22.c
@@ -25,6 +25,9 @@ void pmic_init(void)
 	unsigned char reg;
 
 	// Needed for speedy to work.
+	// PMU Debug register decides alot of clock stuff (contrary to its name)
+	// Clear selected clock CLKOUT0[13:8], enable CLKOUT0[0] and choose select TICCLK_MUX
+	// For CLKOUT0 (1 << 8) to be interpreted as 0b00_0001 for [13:8]. 
 	writel((readl(0x15860A00) & 0xffffc0fe) | 0x100, 0x15860A00);
 
 	speedy_init(CONFIG_SPEEDY0_BASE);
